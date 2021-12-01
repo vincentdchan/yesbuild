d668f1025cef02cd5f23dae40fca44a702279827
Vincent Chan <okcdz@diverse.space>
2021-12-01 23:47:32 +0800

    feat: UI of visualizer

--- packages/yesbuild-visualizer/src/index.tsx
+++ packages/yesbuild-visualizer/src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  position: fixed;
+	width: 100%;
+	height: 100%;
+  align-items: center;
+`;
 
 function App() {
-	return(
-		<div>Hello world, Yesbuild!</div>
-	);
+  return (
+    <Container>
+      Hello world, Yesbuild!
+    </Container>
+  );
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('app'),
+  <App />,
+  document.getElementById('app'),
 );
